9f0e494f13800802c15c97e5e42006b70ff8ee7d
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-14 19:20:35 -0500

    feat: :sparkles: add topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
+import Topbar from "./components/Topbar"
 
 function App() {
   return (
     <>
-      <p className="hidden lg:block bg-red-400">
-        Ga
-      </p>
-      <p className="lg:hidden bg-green-400">
-        Ra
-      </p>
+      <Topbar/>
     </>
   )
 }
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,28 @@
+import Coin from '../icons/Coin.jsx'
+import Truck from '../icons/Truck.jsx'
+import Warranty from '../icons/Warranty.jsx'
+
+const Topbar = () => {
+  return (
+    <section className="bg-[#304755]">
+      <div className="max-w-[1380px] mx-auto flex gap-6 justify-center text-[#f2ebe9] text-xs py-2">
+        <div className="flex items-center gap-1">
+          <Truck/>
+          <p>Free delivery</p>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <Coin/>
+          <p>Worry-free trial</p>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <Warranty/>
+          <p>Warranty</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Topbar
