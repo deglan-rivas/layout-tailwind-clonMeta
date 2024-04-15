304f0f3cda89c4e9e317f657ad5e9ba539f7736d
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-14 19:08:17 -0500

    feat: :tada: set up project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <>
+      <p className="hidden lg:block bg-red-400">
+        Ga
+      </p>
+      <p className="lg:hidden bg-green-400">
+        Ra
+      </p>
+    </>
+  )
+}
+
+export default App
